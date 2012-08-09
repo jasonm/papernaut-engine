a8c02b8f48cf1e723d1608e486a3c45450989557
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-09 22:27:14 +0200

    Deliver identifier_strings via /discussions API

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,6 +2,15 @@ class DiscussionsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Discussion.identified_by(params['query'])
+    discussions = Discussion.identified_by(params['query'])
+    respond_with(present(discussions))
+  end
+
+  private
+
+  def present(discussions)
+    discussions.map { |discussion|
+      discussion.as_json(methods: %w(identifier_strings))
+    }
   end
 end
