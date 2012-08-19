d0e4cf71fb6e5dce9c516a8b62a104ee9d4cc7f1
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-19 17:42:14 +0200

    Extract Loaders::Base::EntryPage, refactor Loaders::DiscoverMagazine::EntryPage

---
+++ lib/loaders/base.rb
@@ -0,0 +1,52 @@
+module Loaders
+  module Base
+    class EntryPage
+      def initialize(url)
+        @url = url
+      end
+
+      def load
+        begin
+          Discussion.create(url: url, title: title, page_urls: page_urls)
+        rescue Exception => e
+          exception_presentation = "#{e.class} (#{e.message}):\n    " + e.backtrace.join("\n    ") + "\n\n"
+          Loaders.logger.error("#{self.class.name} could not load #{@url}:\n#{exception_presentation}")
+        end
+      end
+
+      private
+
+      def url
+        @url
+      end
+
+      def title
+        doc.css(title_tag_selector)[0].text
+      end
+
+      def page_urls
+        filtered_page_urls
+      end
+
+      def filtered_page_urls
+        Loaders::OutgoingUrlFilter.new(@url, unfiltered_page_urls).filtered
+      end
+
+      def unfiltered_page_urls
+        doc.css(page_links_selector).map { |a| a['href'] }
+      end
+
+      def doc
+        @doc ||= Loaders.get_html(url)
+      end
+
+      def title_tag_selector
+        raise "#{self.class.name} must implement #title_tag_selector"
+      end
+
+      def page_links_selector
+        raise "#{self.class.name} must implement #page_links_selector"
+      end
+    end
+  end
+end
--- lib/loaders/discover_magazine.rb
+++ lib/loaders/discover_magazine.rb
@@ -65,46 +65,8 @@ module Loaders
       end
     end
 
-    class EntryPage # < Loaders::Base::BlogEntryPage
-      def initialize(url)
-        @url = url
-      end
-
-      # TODO: DRY up as template method of sorts? superclass method?
-      def load
-        begin
-          Discussion.create(url: url, title: title, page_urls: page_urls)
-        rescue Exception => e
-          exception_presentation = "#{e.class} (#{e.message}):\n    " + e.backtrace.join("\n    ") + "\n\n"
-          Loaders.logger.error("#{self.class.name} could not load #{@url}:\n#{exception_presentation}")
-        end
-      end
-
+    class EntryPage < Loaders::Base::EntryPage
       private
-
-      def url
-        @url
-      end
-
-      def title
-        doc.css(title_tag_selector)[0].text
-      end
-
-      def page_urls
-        filtered_page_urls
-      end
-
-      def filtered_page_urls
-        Loaders::OutgoingUrlFilter.new(@url, unfiltered_page_urls).filtered
-      end
-
-      def unfiltered_page_urls
-        doc.css(page_links_selector).map { |a| a['href'] }
-      end
-
-      def doc
-        @doc ||= Loaders.get_html(url)
-      end
 
       def title_tag_selector
         'div.weblog-entry h2 a'
