bd0a4a6def896889b568e8ccfd378ed1d3c8aa9a
Jason Morrison <jason.p.morrison@gmail.com>
2012-08-20 21:38:35 +0200

    more stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,11 @@
 class StatsController < ApplicationController
   def index
     render json: {
+       indexed_discussions: Discussion.count,
+       indexed_pages: Page.count,
        identified_discussions: Discussion.joins(pages: :identifiers).count(distinct: true),
-       identified_pages: Page.joins(:identifiers).count(distinct: true)
+       identified_pages: Page.joins(:identifiers).count(distinct: true),
+       distinct_identification_tags: Identifier.count
     }
   end
 end
