ccffb3f56bec1d9c6978da9651ce6917cd869585
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-05 11:43:11 -0800

    Require ENV['SECRET_TOKEN'] in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Journalclub::Application.config.secret_token = 'aac08ebd3c7a5416fe1de1163bc889953562b75a387b3e5f80ea02550b0f8d921b957b0de30202ddc9478741a67e67233b67a266885a2ed41d23a4b0eab9f78a'
+Journalclub::Application.config.secret_token = if Rails.env.production?
+  ENV['SECRET_TOKEN'] || raise("Set ENV['SECRET_TOKEN'] for production")
+else
+  'aac08ebd3c7a5416fe1de1163bc889953562b75a387b3e5f80ea02550b0f8d921b957b0de30202ddc9478741a67e67233b67a266885a2ed41d23a4b0eab9f78a'
+end
